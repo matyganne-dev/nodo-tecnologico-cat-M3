ded185c771d69625fea56c7cc584753fd486ad7c
Ganne Moreno Pablo Matias <matyganne@gmail.com>
2026-02-25 19:40:21 -0300

    paso a paso modulo 3 fs

---
+++ Sprint1/Clase2/fs-ejemplo/index.mjs
@@ -0,0 +1,20 @@
+import fs from 'fs';
+
+// Leer un archivo de manera asincrona
+fs.readFile('./data/example.txt', 'utf8', (err, data) =>{
+    if (err) throw err;
+    console.log('Contenido del archivo:', data);
+});
+
+// Escribir en un nuevo archivo
+fs.writeFile('./data/newfile.txt', 'Contenido nuevo', (err)=>{
+    if (err) throw err;
+    console.log('Archivo creado exitosamente');
+})
+
+// Renombrar un archivo
+fs.rename('./data/newfile.txt', './data/renamedfile.txt', (err)=>{
+    if (err) throw err;
+    console.log('Archivo renombrado');
+})
+
