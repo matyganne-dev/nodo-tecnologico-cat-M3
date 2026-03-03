c9112b3f87815350d273c075dfd3db4d1de7a053
Ganne Moreno Pablo Matias <matyganne@gmail.com>
2026-03-03 04:30:57 -0300

    Se agrego rutas de prueba con express, para el home y data

--- Sprint1/Clase4/practica_Express/app.js
+++ Sprint1/Clase4/practica_Express/app.js
@@ -17,3 +17,17 @@ app.listen(PORT, () => {
     console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
 
+
+
+// Ruta GET para el home
+// Solicitud: http://localhost:3000/
+app.get('/', (req, res) => {
+    res.send('Página de inicio');
+});
+
+// Ruta GET para recibir datos simples
+// Solicitud: http://localhost:3000/data
+app.get('/data', (req, res) => {
+    res.send('Datos recibidos');
+});
+
