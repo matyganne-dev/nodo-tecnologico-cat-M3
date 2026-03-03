c1ab16d518287cebbfcc06105dd6bad0558d56cf
Ganne Moreno Pablo Matias <matyganne@gmail.com>
2026-03-03 04:16:07 -0300

    configuracion basica del servidor

---
+++ Sprint1/Clase4/practica_Express/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+// Crear una instancia de Express
+const app = express();
+
+// Configurar el puerto en el que el servidor escuchara
+const PORT = 3000;
+
+// Ruta basica
+app.get('/', (req, res) => {
+    res.send('¡Hola, mundo!');
+});
+
+// Iniciar el servidor
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
+
