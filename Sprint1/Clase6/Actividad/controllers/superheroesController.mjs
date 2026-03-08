75be69e545a117228688095a8e045d0a5241c3a8
Ganne Moreno Pablo Matias <matyganne@gmail.com>
2026-03-08 09:50:29 -0300

    controlador

---
+++ Sprint1/Clase6/Actividad/controllers/superheroesController.mjs
@@ -0,0 +1,27 @@
+import {obtenerSuperheroePorId, buscarSuperHeroesPorAtributo, obtenerSuperheroesMayoresDe30} from '../services/superheroesService.mjs';
+import { renderizarSuperheroe, renderrizarSuperheroes } from '../views/responseView.mjs';
+
+export function obtenerSuperheroePorIdController(req, res){
+    const {id} = req.params;
+    const superheroe = obtenerSuperheroePorId(parseInt(id));
+    if(superheroe){
+        res.send(renderizarSuperheroe(superheroe));
+    } else {
+        res.status(404).send('Superhéroe no encontrado');
+    }
+}
+
+export function buscarSuperheroesPorAtributoController(req, res){
+    const {atributo, valor} = req.params;
+    const superheroes = buscarSuperHeroesPorAtributo(atributo, valor);
+    if(superheroes.length > 0){
+        res.send(renderrizarSuperheroes(superheroes));
+    } else {
+        res.status(404).send('No se encontraron superhéroes con ese atributo y valor');
+    }
+}
+
+export function obtenerSuperheroesMayoresDe30Controller(req, res){
+    const superheroes = obtenerSuperheroesMayoresDe30();
+    res.send(renderizarListaSuperheroes(superheroes));
+}
