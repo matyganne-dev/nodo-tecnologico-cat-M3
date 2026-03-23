8cbbe68d6b8b3a62ce8da5ef2b820c24b08c2a09
Ganne Moreno Pablo Matias <matyganne@gmail.com>
2026-03-23 19:44:06 -0300

    creacion y configuracion de dbConfig.mjs para conectar a mongodb

---
+++ Sprint2/Actividad3/src/config/dbConfig.mjs
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+export async function connecBD() {
+    try {
+        await mongoose.connect('mongodb+srv://grupo-06:grupo-06@cluster0.blryo.mongodb.net/NodeMod3Cohorte5');
+        console.log('Conexcion exitosa a MongoDB');
+    } catch (error) {
+        console.error('Error al conectar a MongoDB', error);
+        process.exit(1);
+    }
+}
