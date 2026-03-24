b7003cfcd6bcc21d610120257530afcc009e8b92
Ganne Moreno Pablo Matias <matyganne@gmail.com>
2026-03-23 22:47:07 -0300

    modificacion de archivo superheroroutes por inconveniente en la entrada ya que se prioriza las entradas dinamicas

--- Sprint2/Actividad3/src/routes/superHeroRoutes.mjs
+++ Sprint2/Actividad3/src/routes/superHeroRoutes.mjs
@@ -9,8 +9,8 @@ import {
 const router = express.Router();
 
 router.get('/heroes', obtenerTodosLosSuperheroesController);
-router.get('/heroes/:id', obtenerSuperheroePorIdController);
-router.get('/heroes/buscar/:atributo/:valor', buscarSuperheroesPorAtributoController);
 router.get('/heroes/mayores-30', obtenerSuperheroesMayoresDe30Controller);
+router.get('/heroes/buscar/:atributo/:valor', buscarSuperheroesPorAtributoController);
+router.get('/heroes/:id', obtenerSuperheroePorIdController);
 
 export default router;
