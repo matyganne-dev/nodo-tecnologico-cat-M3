acde8bd63f467b9569c1a5ee7a2ca99cd1726128
Ganne Moreno Pablo Matias <matyganne@gmail.com>
2026-04-07 00:17:52 -0300

    cambion en el mensaje de conexion en dbconfig.js

--- Sprint2/Actividad3/src/config/dbConfig.mjs
+++ Sprint2/Actividad3/src/config/dbConfig.mjs
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 export async function connecBD() {
     try {
         await mongoose.connect('mongodb+srv://grupo-06:grupo-06@cluster0.blryo.mongodb.net/NodeMod3Cohorte5');
-        console.log('Conexcion exitosa a MongoDB');
+        console.log('Conexion exitosa a MongoDB');
     } catch (error) {
         console.error('Error al conectar a MongoDB', error);
         process.exit(1);
