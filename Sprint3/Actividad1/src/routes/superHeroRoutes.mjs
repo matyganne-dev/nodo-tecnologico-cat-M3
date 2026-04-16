589760055cc9458bcfe1e5b95ed931e2469cf938
Ganne Moreno Pablo Matias <matyganne@gmail.com>
2026-04-16 04:56:23 -0300

    Configuración de endpoints para POST, PUT y DELETE

--- Sprint3/Actividad1/src/routes/superHeroRoutes.mjs
+++ Sprint3/Actividad1/src/routes/superHeroRoutes.mjs
@@ -3,14 +3,34 @@ import {
     obtenerSuperheroePorIdController,
     obtenerTodosLosSuperheroesController,
     buscarSuperheroesPorAtributoController,
-    obtenerSuperheroesMayoresDe30Controller
+    obtenerSuperheroesMayoresDe30Controller,
+    crearSuperheroeController,
+    actualizarSuperheroeController,
+    borrarSuperheroeController,
+    borrarSuperheroePorNombreController
 } from '../controllers/superheroesController.mjs';
 
 const router = express.Router();
 
+//Rutas (GET)
 router.get('/superheroes', obtenerTodosLosSuperheroesController);
 router.get('/superheroes/edad/mayorA30', obtenerSuperheroesMayoresDe30Controller);
 router.get('/superheroes/atributo/:atributo/:valor', buscarSuperheroesPorAtributoController);
 router.get('/superheroes/id/:id', obtenerSuperheroePorIdController);
+
+//POST
+router.post('/superheroes', crearSuperheroeController);
+
+//PUT
+//actualizacion superheroe ID
+router.put('/superheroes/id/:id', actualizarSuperheroeController);
+
+//DELETE
+
+//Eliminacion superheroe por ID
+router.delete('/superheroes/id/:id', borrarSuperheroeController);
+
+//Eliminacion superheroe por Nombre
+router.delete('/superheroes/nombre/:nombre', borrarSuperheroePorNombreController);
 
 export default router;
