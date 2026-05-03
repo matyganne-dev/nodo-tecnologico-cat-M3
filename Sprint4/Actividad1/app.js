ed92dc16694adbd59b7c263b4d359ebe06c3fdd5
Ganne Moreno Pablo Matias <matyganne@gmail.com>
2026-05-03 00:59:34 -0300

    feat: configurar express-ejs-layouts y definir estructura de layout base

--- Sprint4/Actividad1/app.js
+++ Sprint4/Actividad1/app.js
@@ -1,5 +1,7 @@
 
 import express from 'express';
+import expressLayouts from 'express-ejs-layouts';
+import path from 'path';
 import { setServers } from 'node:dns/promises';
 import { connecBD } from './src/config/dbConfig.mjs';
 import superHeroRoutes from './src/routes/superHeroRoutes.mjs';
@@ -14,16 +16,26 @@ const PORT = process.env.PORT || 3005;
 //Se establece el motor de plantillas ejs
 app.set('view engine', 'ejs');
 // Esta línea sirve para encontrar la carpeta correctamente
-app.set('views', './src/views');
+app.set('views', path.resolve('src/views'));// Usa path.resolve para evitar errores de ruta
 
 // Middleware para parsear JSON
 app.use(express.json());
+
+// Activar los layouts
+app.use(expressLayouts);
+
+// Indicar que el archivo base es layout.ejs (que debe estar en src/views/layout.ejs)
+app.set('layout', 'layout');
+
+// Servir archivos estáticos (para CSS e imágenes)
+app.use(express.static('public'));
 
 //(ETAPA 3) -> Middleware para parsear datos de formularios (Vistas EJS)
 //recibe el texto y lo convierte a un objeto javascript
 app.use(express.urlencoded({ extended: true }));
 
 //Los formularios HTML nativos solo soportan GET y POST. Para usar un PUT real desde el navegador, tenemos que instalar un middleware llamado method-override
+//Esto tengo que ver de limpiar porque al validar desde el front con el fetch creo que podria sacar esto. (preguntar a profes o chat) 
 app.use(methodOverride('_method'));
 
 // Conexión a MongoDB
